500135e1cd6b0ca060c1a962b3a4d6e8096e4894
kieranochola <ocholadavidk@gmail.com>
2024-12-14 21:59:24 +0300

    dsa pyhton2

---
+++ main.py
@@ -0,0 +1,20 @@
+my_turple=(1,2,3)
+print(my_turple)
+
+my_turple=(1,"hello",2,3)
+print(my_turple)
+
+my_turple=([4,5,6],"hello",(1,2,3))
+print(my_turple)
+
+my_turple=('o','r','a','n','g','e')
+print(my_turple[3])
+print(my_turple[0])
+
+n_tuple=("mouse",[1,2,3,4],(4,5,6))
+
+print(n_tuple[0][4])
+print(n_tuple[1][3])
+
+for letter in(my_turple):
+    print("hello",letter)
---
+++ main2.py
@@ -0,0 +1,4 @@
+myset={1,2,3,4,55,5,56,6,6,66,4,3,3,3}
+myset2={1,1,1,1,1,1,2,33,4,4,4,4,4,4}
+print(myset.difference(myset2))
+print(myset.symmetric_difference(myset2))
